0d37bc75ca7af8accf7907b838ceb2defc82c185
A-Jar-Of-Peanuts <ajarofpeanuts@gmail.com>
2024-03-09 14:56:00 -0800

    m

--- frontend/src/News.js
+++ frontend/src/News.js
@@ -1,16 +1,103 @@
-import { Button, Flex } from 'antd';
 import React from 'react';
+import { Breadcrumb, Layout, Menu, theme, Button} from 'antd';
+import { Input } from 'antd';
+import { useNavigate } from "react-router-dom";
+
+const { TextArea } = Input;
+const { Header, Content, Footer, Sider} = Layout;
+const items = new Array(1).fill(null).map((_, index) => ({
+  key: String(index + 1),
+  label: `Main Menu`,
+}));
 
 function News() {
-  return (
-         <Flex gap="small" wrap="wrap">
-    <Button type="primary">Primary Button</Button>
-    <Button>Default Button</Button>
-    <Button type="dashed">Dashed Button</Button>
-    <Button type="text">Text Button</Button>
-    <Button type="link">Link Button</Button>
-  </Flex>
-  );
+    const navigate = useNavigate();
+    function mm(e) {
+        console.log("hello");
+        navigate("/app");
+    }
+    function analyze(e) {
+
+    }
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+      } = theme.useToken();
+      return (
+        <Layout>
+          <Header
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              color: 'white',
+            }}
+          >
+            <div className="demo-logo" />
+            <h1>ELECTION GUARD</h1>
+
+            <Menu onClick={mm} theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}>
+            <Menu.Item key="Main Menu" >
+                <label className="nav-text">Main Menu</label>
+            </Menu.Item>
+            </Menu>
+
+          </Header>
+          <Content
+            style={{
+              padding: '0 48px',
+            }}
+          >
+            <Layout
+              style={{
+                padding: '24px 0',
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              <Sider
+                style={{
+                  background: colorBgContainer,
+                }}
+                width={200}
+              >
+                <Menu
+                  mode="inline"
+                  defaultSelectedKeys={['1']}
+                  defaultOpenKeys={['sub1']}
+                  style={{
+                    height: '100%',
+                  }}
+                  items={items}
+                />
+              </Sider>
+              <Content
+                style={{
+                  padding: '0 24px',
+                  minHeight: 280,
+                }}
+              >
+               
+               
+            <Input placeholder="URL Link" />
+            <br />
+            <br />
+            <TextArea rows={20} placeholder="Content"/>            
+            <br />
+            <br/>
+            <Button type="primary" onClick={analyze} >ANALYZE</Button> 
+
+              </Content>
+            </Layout>
+          </Content>
+          <Footer
+            style={{
+              textAlign: 'center',
+            }}
+          >
+          </Footer>
+        </Layout>
+      );
 }
 
 export default News;
