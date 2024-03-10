a5fa46a3cefd2af2558831bf73506712fe94b8f4
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2024-03-09 18:44:13 -0800

    update node module

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import News from './News.js';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={"/"}>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/' element={<News />} />
-      </Routes>
-    </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
