b366c665bf14c178827498301845461281f89cae
Alice <cyang41@students.cs.ubc.ca>
2024-03-10 09:22:40 -0700

    cover lang

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,25 +42,53 @@ function App() {
       navigate("/");
     }
 
+    const langVal = "en";
+    // const titleY = "Your";
+    // const titleT = "Trust";
+    // const titleR = "Radar";
+
     const handleChange = (value) => {
       if(value == "en") {
+         //langVal = "en";
+        //  titleY = "Your";
+        //  titleT = "Truth";
+        //  titleR = "Radar";
+
+        setTitleY("Your")
+        setTitleT("Truth");
+        setTitleR("Radar");
         setButton("Fake news? Let's check!");
         setTitle("What is Election Guard?");
         setDescr("Welcome to Election Guard! The platform integrates sentiment analysis, semantic disambiguation for precise truth discernment. Additionally, we have included additional languages for fake news detection for greater coverage and inclusivity. We ensure immutable transparency and inclusivity, safeguarding against digital deception. Join us in navigating the information landscape with clarity and confidence.");
+        <FrontLanding onClick={mm} button={button} value={langVal} y={titleY} t={titleT} r={titleR}/>
 
 
       } else if (value == "fr") {
+        //langVal = "fr";
+        // titleY = "Votre";
+        // titleT = "radar";
+        // titleR = "de confiance";
         setButton("faux article de presse ? Vérifions !");
         setTitle("Qu'est-ce que Election Guard?");
         setDescr("Bienvenue à Election Guard ! La plateforme intègre l'analyse des sentiments, la désambiguïsation sémantique pour un discernement précis de la vérité. En outre, nous avons inclus des langues supplémentaires pour la détection des fausses nouvelles afin d'assurer une plus grande couverture et une plus grande inclusivité. Nous garantissons une transparence et une inclusivité immuables, en nous protégeant contre la tromperie numérique. Rejoignez-nous pour naviguer dans le paysage de l'information avec clarté et confiance.");
-
+        setTitleY("Votre");
+        setTitleT("radar");
+        setTitleR("de confiance");
+        <FrontLanding onClick={mm} button={button} value={langVal} y={titleY} t={titleT} r={titleR}/>
 
 
       } else if (value == "sp") {
+        //langVal = "sp";
+        // titleY = "Su";
+        // titleT = "radar";
+        // titleR = "de confianza";
         setButton("¿Noticias falsas? Vamos a comprobarlo.");
         setTitle("¿Qué es la Election Guard?");
         setDescr("Bienvenido a Election Guard La plataforma integra análisis de sentimientos y desambiguación semántica para un discernimiento preciso de la verdad. Además, hemos incluido idiomas adicionales para la detección de noticias falsas para una mayor cobertura e inclusividad. Garantizamos transparencia e inclusividad inmutables, protegiendo contra el engaño digital. Únase a nosotros para navegar por el panorama informativo con claridad y confianza.");
-
+        setTitleY("Su");
+        setTitleT("radar");
+        setTitleR("de confianza");
+        <FrontLanding onClick={mm} button={button} value={langVal} y={titleY} t={titleT} r={titleR}/>
 
 
       }
@@ -69,6 +97,9 @@ function App() {
   const [button, setButton] = useState("Fake news? Let's check!");
   const [title, setTitle] = useState("What is Election Guard?");
   const [descr, setDescr] = useState("Welcome the Election Guard! The platform integrates sentiment analysis, semantic disambiguation for precise truth discernment. Additionally, we have included additional languages for fake news detection for greater coverage and inclusivity. We ensure immutable transparency and inclusivity, safeguarding against digital deception. Join us in navigating the information landscape with clarity and confidence.");
+  const [titleY, setTitleY] = useState("Your");
+  const [titleT, setTitleT] = useState("Truth");
+  const [titleR, setTitleR] = useState("Radar");
 
   const {
     token: { colorBgContainer, borderRadiusLG },
@@ -115,7 +146,7 @@ function App() {
               ]}
           />
       </Header>
-      <FrontLanding onClick={mm} button={button} />
+      <FrontLanding onClick={mm} button={button} value={langVal} y={titleY} t={titleT} r={titleR}/>
       <Content style={{ padding: '0',}}>
         
         <div
@@ -148,12 +179,10 @@ function App() {
             borderRadius: borderRadiusLG,
           }}
         ></div>
-          <div style={{marginLeft: '190px'}}>
           <h1 style={{color: '#00008B', fontSize: '50px'}}> {title}</h1>
-          <h5 style={{color: '#00008B', fontSize: '20px', width: '1000px'}}>
+          <h5 style={{color: '#00008B', fontSize: '20px', maxWidth: '1000px'}}>
             {descr}
           </h5>
-          </div>
           </Flex>
 
         
