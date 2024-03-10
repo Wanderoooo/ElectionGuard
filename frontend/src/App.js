ec76b42c3fa57ae1a2050576e3ae1f3461280de8
Alice <cyang41@students.cs.ubc.ca>
2024-03-10 09:30:23 -0700

    de

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -73,7 +73,7 @@ function App() {
         setDescr("Bienvenue à Election Guard ! La plateforme intègre l'analyse des sentiments, la désambiguïsation sémantique pour un discernement précis de la vérité. En outre, nous avons inclus des langues supplémentaires pour la détection des fausses nouvelles afin d'assurer une plus grande couverture et une plus grande inclusivité. Nous garantissons une transparence et une inclusivité immuables, en nous protégeant contre la tromperie numérique. Rejoignez-nous pour naviguer dans le paysage de l'information avec clarté et confiance.");
         setTitleY("Votre");
         setTitleT("Radar");
-        setTitleR("de Confiance");
+        setTitleR("Confiance");
         <FrontLanding onClick={mm} button={button} value={langVal} y={titleY} t={titleT} r={titleR}/>
 
 
@@ -87,7 +87,7 @@ function App() {
         setDescr("Bienvenido a Election Guard La plataforma integra análisis de sentimientos y desambiguación semántica para un discernimiento preciso de la verdad. Además, hemos incluido idiomas adicionales para la detección de noticias falsas para una mayor cobertura e inclusividad. Garantizamos transparencia e inclusividad inmutables, protegiendo contra el engaño digital. Únase a nosotros para navegar por el panorama informativo con claridad y confianza.");
         setTitleY("Su");
         setTitleT("Radar");
-        setTitleR("de Confianza");
+        setTitleR("Confianza");
         <FrontLanding onClick={mm} button={button} value={langVal} y={titleY} t={titleT} r={titleR}/>
 
 
